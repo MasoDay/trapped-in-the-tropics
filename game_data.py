ee8f37e5fd311072063fbf28929b709da03b1e89
Ben <budemaso@gmail.com>
2024-02-12 16:04:07 -0700

    Added file paths for CSV files storing level features built in Tiled.

---
+++ game_data.py
@@ -0,0 +1,11 @@
+level_0 = {
+    "terrain": "../pirate-jump/assets/2 - Level/levels/0/level_zero_terrain.csv",
+    "coins": "../pirate-jump/assets/2 - Level/levels/0/level_zero_coins.csv",
+    "fg palms": "../pirate-jump/assets/2 - Level/levels/0/level_zero_fg_palms.csv",
+    "bg palms": "../pirate-jump/assets/2 - Level/levels/0/level_zero_bg_palms.csv",
+    "crates": "../pirate-jump/assets/2 - Level/levels/0/level_zero_crates.csv",
+    "enemies": "../pirate-jump/assets/2 - Level/levels/0/level_zero_enemies.csv",
+    "constrainsts": "../pirate-jump/assets/2 - Level/levels/0/level_zero_constraints.csv",
+    "player": "../pirate-jump/assets/2 - Level/levels/0/level_zero_player.csv",
+    "grass": "../pirate-jump/assets/2 - Level/levels/0/level_zero_grass.csv"
+}

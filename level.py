eb40604e1835a4fc83ce1653e11da54e7fd85876
Ben <budemaso@gmail.com>
2024-02-12 16:54:42 -0700

    Terrain map show up

--- level.py
+++ level.py
@@ -1,13 +1,13 @@
 import pygame
 from support import import_csv_layout, import_cut_graphics
 from settings import tile_size
-from tiles import Tile
+from tiles import Tile, StaticTile
 
 class Level:
     def __init__(self, level_data, surface):
         # General setup
         self.display_surface = surface
-        self.world_shift = 0
+        self.world_shift = -4
 
         # Terrain setup
         terrain_layout = import_csv_layout(level_data["terrain"])
@@ -27,7 +27,7 @@ class Level:
                             "../pirate-jump/assets/2 - Level/graphics/terrain/terrain_tiles.png"
                             )
                         tile_surface = terrain_tile_list[int(value)]
-                        sprite = Tile(tile_size, x, y)
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
                         sprite_group.add(sprite)
 
         return sprite_group

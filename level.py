5e17243f0307ecaa4481b8bbd738dfdadf6cb348
Ben <budemaso@gmail.com>
2024-02-12 15:57:27 -0700

    Building out the run method.

---
+++ level.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Level:
+    def __init__(self, level_data, surface):
+        self.display_surface
+
+    def run (self):
+        # will start running the level
+        pass
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pygame, sys
 from settings import *
 # from tiles import Tile
-# from level import Level
+from level import Level
 
 # PyGame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-# level = Level(level_map, screen)
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
@@ -16,7 +16,7 @@ while True:
             sys.exit()
 
     screen.fill("black")
-    # level.run()
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
